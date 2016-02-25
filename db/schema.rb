98b7c9d291e33427cd1d2382e6fac601912eb9e4
amiel asparen <amielasparen@gmail.com>
2016-02-26 02:57:49 +0800

    2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225183557) do
+ActiveRecord::Schema.define(version: 20160225183839) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20160225183557) do
     t.integer  "opdur"
     t.integer  "nordur"
     t.integer  "pesdur"
+    t.string   "activityID"
   end
 
   create_table "profiles", force: :cascade do |t|
